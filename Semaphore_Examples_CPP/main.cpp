47d7fb3d1a29b4d18002ef8691cb8b6001550a86
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-08-18 23:50:43 +0300

    update main

--- Semaphore_Examples_CPP/main.cpp
+++ Semaphore_Examples_CPP/main.cpp
@@ -8,37 +8,36 @@ using namespace classical_synchronization_problems;
 
 int main()
 {
-/*
     // INTRODUCTION
-     serialization::run();
-     non_determinism::run();
-     concurrent_writes::run();
-     concurrent_updates::run();
+//    serialization::run();
+//     non_determinism::run();
+//     concurrent_writes::run();
+//     concurrent_updates::run();
 
     // BASIC SYNCH PATTERNS
-     signaling::run();
-     rendezvous::run();
-     rendezvous_deadlock::run();
-     mutex::run();
-     multiplex::run();
-     barrier_deadlock::run();
-     barrier_solution::run();
-     barrier_deadlock_2::run();
-     reusable_barrier_deadlock::run();
-     reusable_barrier_deadlock_2::run();
-     reusable_barrier_solution::run();
-     preloaded_turnstile::run();
-     barrier_object::run();
-     leader_and_follower_queue::run();
- */
+//     signaling::run();
+//     rendezvous::run();
+//     rendezvous_deadlock::run();
+//     mutex::run();
+//     multiplex::run();
+//     barrier_deadlock::run();
+//     barrier_solution::run();
+//     barrier_deadlock_2::run();
+//     reusable_barrier_deadlock::run();
+//     reusable_barrier_deadlock_2::run();
+//     reusable_barrier_solution::run();
+//     preloaded_turnstile::run();
+//     barrier_object::run();
+//     leader_and_follower_queue::run();
 
     // CLASSICAL SYNCH PROBLEMS
-    // producer_consumer_problem_infinite::run();
-    // producer_consumer_problem_finite::run();
-    // readers_and_writers_problem::run();
-    // readers_and_writers_problem::run();
-    // no_starving_mutex::run();
-    dining_philosophers::run();
+//     producer_consumer_problem_infinite::run();
+//     producer_consumer_problem_finite::run();
+//     readers_and_writers_problem::run();
+//     readers_and_writers_problem::run();
+//     no_starving_mutex::run();
+//     dining_philosophers::run();
+//     cigarette_smokers_parnas_solution::run();
 
     return 0;
 }
